74cf38d9bcbfa978cf96e8cca73b5a3d8f9691c0
senatrevor <88690592+senatrevor@users.noreply.github.com>
2021-10-31 20:38:43 -0700

    Third update (Part 1)
    This commit includes major changes to css (updated layout so that content scales with window) and added heads (85 ). Part 2 will include new faces.

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ window.onload = function ()
 
 // FACE
 var face = new Image();
-var faceNum = Math.floor( Math.random() * 24 )+1;
-var faceName="images/face" + faceNum + ".png";
+var faceNum = Math.floor( Math.random() * 85 )+1;
+var faceName="faces/face" + faceNum + ".png";
 face.src = faceName;
 
 // HEAD
 var head = new Image();
-var headNum = Math.floor( Math.random() * 24 )+1;
-var headName="images/head" + headNum + ".png";
+var headNum = Math.floor( Math.random() * 85 )+1;
+var headName="heads/head" + headNum + ".png";
 head.src = headName;
 
     // Wait until slime part is loaded and then call the function that displays all images
@@ -26,9 +26,9 @@ face.onload = function () {
 head.onload = function () {
     buildRobot();
 }
-    // display all images
-    function buildRobot()
-    {
+// display all images
+function buildRobot()
+  {
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
     canvas.width = 1200;
@@ -40,6 +40,6 @@ head.onload = function () {
     // FACE
     ctx.drawImage(face, ((1200-face.width)/2), 0);
 
-    }
+  }
 
 }
